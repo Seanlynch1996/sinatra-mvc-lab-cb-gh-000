8647d0e9b369d496927cc049203741d6efebfebf
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-09 22:36:59 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,7 @@ class PigLatinizer
   def piglatinize(text_)
     i = 0
     text = text_
-    text.each do |el|
+    text.each_char do |el|
       if (el == "a" || el == "e" || el == "i" || el == "o" || el == "u" || el == "A" || el == "E" ||
         el == "I" || el == "O" || el == "O")
         cons = i
