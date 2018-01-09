c0e3f3fc2b8c4e974925bd6d2db5cb9b79877d91
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-09 22:14:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @analyzed = params[:input]
+    pig_lat = PigLatinizer.new(@analyzed)
+    @result = pig_lat.piglatinize
+    erb :final
+  end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,23 @@
+
+class PigLatinizer
+
+  attr_accessor :text
+  def initialize
+    #@text = text
+  end
+
+  def piglatinize(text_)
+    #text = @text
+    text = text_
+    text[text.length] = text[0]
+
+    i = 0
+    len = text.length
+    while i < len - 1
+      text[i] = text[i+1]
+      i += 1
+    end
+    text[len - 1] = "ay"
+    text
+  end
+end
