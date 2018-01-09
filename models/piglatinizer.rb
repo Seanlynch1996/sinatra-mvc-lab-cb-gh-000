b5ba5b7aeb0f53eb7cea2505743c9190ca869411
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-09 22:32:37 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,28 +8,7 @@ class PigLatinizer
 
   def piglatinize(text_)
     text = text_
-    cons = 0
-    if text.length == 1 || text[0] =~ /aeiouAEIOU/
-      cons = 0
-    elsif text[1] =~ /aeiouAEIOU/
-      cons = 1
-    else
-      cons = 2
-    end
-
-
-    text[text.length] = text[0]
-    i = 0
-    len = text.length
-    while i < len - 1
-      text[i] = text[i+1]
-      i += 1
-    end
-    if text[len - 1] =~ /aeiouAEIOU/
-      text[len - 1] = "way"
-    else
-      text[len - 1] = "ay"
-    end
-    text
+    cons = text[/[aeiouAEIOU/]]
+    cons
   end
 end
